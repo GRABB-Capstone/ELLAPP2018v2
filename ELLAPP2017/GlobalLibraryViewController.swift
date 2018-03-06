9c5b17d1b3be1ad31dc2dd19aeb283315301814d
Nick Ponce <nickponce5@gmail.com>
2018-03-06 13:31:38 -0800

    Added the Parse/UI subspec and started up the library tableviews

--- ELLAPP2017/GlobalLibraryViewController.swift
+++ ELLAPP2017/GlobalLibraryViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import Parse
 
 class GlobalLibraryViewController: UIViewController {
 
     @IBOutlet weak var globalLibraryTableView: UITableView!
     
+    var titles = [String]()
+    var authors = [String]()
+    var coverPictures = [PFFile]()
+    var selectedTitle = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        // Get all book objects and extract their data into arrays
+        getAllBooks() {
+            (books: [PFObject]) in
+            for book in books {
+                self.titles.append(book["name"] as! String)
+                self.authors.append(book["author"] as! String)
+                self.coverPictures.append(book["coverPicture"] as! PFFile)
+                
+                self.globalLibraryTableView.reloadData()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +40,21 @@ class GlobalLibraryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // Support functions -> To be replaced by modular functions - NAP
+    
+    func getAllBooks(completion: @escaping (_ result: [PFObject]) -> Void) {
+        // Get the PFQuery<Object> for the Books class
+        let bookQuery = PFQuery(className: "Book")
+        
+        bookQuery.findObjectsInBackground(block: { (books: [PFObject]?, error: Error?) -> Void in
+            if let result = books {
+                completion(result)
+            }
+            else {
+                completion([])
+            }
+        })
+    }
 
     /*
     // MARK: - Navigation
@@ -34,4 +66,33 @@ class GlobalLibraryViewController: UIViewController {
     }
     */
 
+}
+
+extension GlobalLibraryViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = globalLibraryTableView.dequeueReusableCell(withIdentifier: "global_library_cell", for: indexPath) as? GlobalLibraryTableViewCell
+        
+        // Add cell initialization
+        cell?.globalBookTitleLabel.text = titles[indexPath.row]
+        cell?.globalAuthorLabel.text = authors[indexPath.row]
+        
+        cell?.globalCoverPictureImageView.image = UIImage(named: "placeholder.jpg")
+        cell?.globalCoverPictureImageView.file = coverPictures[indexPath.row]
+        cell?.globalCoverPictureImageView.loadInBackground()
+        
+        return cell!
+    }
+    
+}
+extension GlobalLibraryViewController: UITableViewDelegate {
+    
 }
---
+++ ELLAPP2017/LocalLibraryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocalLibraryTableViewCell.swift
+//  ELLAPP2017
+//
+//  Created by Nick Ponce on 3/6/18.
+//  Copyright © 2018 Ellokids. All rights reserved.
+//
+
+import UIKit
+
+class LocalLibraryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
